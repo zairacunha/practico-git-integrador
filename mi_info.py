3bb995f61cdde7e42b340266d23065ef16eb5816
Zaira Cunha <zairacunhadasilva@gmail.com>
2026-04-07 01:05:55 -0300

    Add files via upload

---
+++ mi_info.py
@@ -0,0 +1,7 @@
+# mi_info.py - Información personal
+nombre = "Zaira Cunha"
+edad = 21
+materia = "Software Factory"
+print(f"Hola, soy {nombre}")
+print(f"Tengo {edad} años")
+print(f"Estoy cursando {materia}")
