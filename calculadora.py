9e2fc6561a0d47d524d0f1b90559c38c22ec37d9
Zaira Cunha <zairacunhadasilva@gmail.com>
2026-05-11 20:13:13 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,5 @@
+# calculadora.py
+num1 = float(input("Ingresá el primer número: "))
+num2 = float(input("Ingresá el segundo número: "))
+suma = num1 + num2
+print(f"La suma de {num1} + {num2} = {suma}")
